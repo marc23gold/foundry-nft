f26745231619e429f0ad67f5dff1138aa97ea1fc
Marcx <haitianhero6@gmail.com>
2023-09-06 03:26:19 -0400

    refactor

---
+++ src/BasicNft.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+
+uint256 private s_tokenCounter;
+
+constructor() ERC721("Dogie", "Dog") {
+    s_tokenCounter = 0;
+}
+
+
+function mintNft() public {
+
+}
+
+
+ function getTokenCounter() external view returns (uint256) {
+    return s_tokenCounter;
+}
+    
+
+}
